a90f5d1dde1067b2011b3cb6949c8d7b175905c2
Jovan Jester <jovan@jester.cafe>
2021-12-01 23:25:20 -0600

    initial commit

---
+++ src/cljsheets/core.cljs
@@ -0,0 +1,38 @@
+(ns cljsheets.core
+  (:require
+   [reagent.dom :as rdom]
+   [re-frame.core :as re-frame]
+   [re-pressed.core :as rp]
+   [breaking-point.core :as bp]
+   [cljsheets.events :as events]
+   [cljsheets.routes :as routes]
+   [cljsheets.views :as views]
+   [cljsheets.config :as config]
+   ))
+
+
+(defn dev-setup []
+  (when config/debug?
+    (println "dev mode")))
+
+(defn ^:dev/after-load mount-root []
+  (re-frame/clear-subscription-cache!)
+  (let [root-el (.getElementById js/document "app")]
+    (rdom/unmount-component-at-node root-el)
+    (rdom/render [views/main-panel] root-el)))
+
+(defn init []
+  (routes/start!)
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (re-frame/dispatch-sync [::rp/add-keyboard-event-listener "keydown"])
+  (re-frame/dispatch-sync [::bp/set-breakpoints
+                           {:breakpoints [:mobile
+                                          768
+                                          :tablet
+                                          992
+                                          :small-monitor
+                                          1200
+                                          :large-monitor]
+                            :debounce-ms 166}])
+  (dev-setup)
+  (mount-root))
---
+++ src/cljsheets/db.cljs
@@ -0,0 +1,4 @@
+(ns cljsheets.db)
+
+(def default-db
+  {:name "re-frame"})
